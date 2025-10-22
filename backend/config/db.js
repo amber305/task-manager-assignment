ae477050f998796a9510de3d8b0d632ce05afd55
MehulGoyal07 <mehulgoyal107@gmail.com>
2025-10-22 10:28:58 +0530

    final Product

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+dotenv.config()
+
+const { MONGO_USER, MONGO_PASS, MONGO_CLUSTER, MONGO_DB } = process.env
+const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_CLUSTER}/${MONGO_DB}?retryWrites=true&w=majority`
+
+export const connectDB = async() => {
+    try {
+        await mongoose.connect(uri)
+        console.log('DB CONNECTED')
+    } catch (err) {
+        console.error('DB CONNECTION ERROR:', err)
+    }
+}
